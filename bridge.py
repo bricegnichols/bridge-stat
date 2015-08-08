c45903decc794725e6aab738c3e11cd24d8f084d
Your Name <you@example.com>
2015-08-08 12:47:40 -0700

    cleaned up main script

--- bridge.py
+++ bridge.py
@@ -1,4 +1,5 @@
 # get Seattle bridge open/close status from Twitter 
+# polling approach, not RESTful, which is ideal
 import twitter
 import pytz
 import datetime
@@ -8,6 +9,7 @@ import email
 from email.mime.text import MIMEText
 from datetime import datetime
 from dateutil.tz import tzoffset
+import sched, time
 
 api = twitter.Api(consumer_key=consumer_key,
 	consumer_secret=consumer_secret,
@@ -30,13 +32,15 @@ def to_local_time(tweet_time_string):
 
     """
     timestamp = email.utils.mktime_tz(email.utils.parsedate_tz(tweet_time_string))
-    return datetime.fromtimestamp(timestamp, pytz.timezone(local_tz)).strftime('%Y-%m-%d %H:%M:%S')
+    
+    return datetime.fromtimestamp(timestamp, pytz.timezone(local_tz)).strftime('%a %b %d %H:%M:%S %Y')
 
 def days_hours_minutes(td):
     mydict = {}
     mydict['days'] = td.days
     mydict['hours'] = td.seconds//3600
     mydict['minutes'] = (td.seconds//60)%60
+    
     return mydict
 
 def filterClosures(bridgeName, handle):
@@ -44,87 +48,76 @@ def filterClosures(bridgeName, handle):
 	closures = getClosures(bridgeName, statuses)
 	currentDate = datetime.now()
 
-	# for status in statuses:
-	# 	print status
-	# 	print status.created_at
-	# 	tweet_time = status.created_at
-	# 	local_time = to_local_time(tweet_time)
-	# 	print time_string
+	for status in statuses:
+		#print status
+		print status.created_at
+		# tweet_time = status.created_at
+		# local_time = to_local_time(tweet_time)
+		# print time_string
 
 	for closure in closures:
 		#Get closures from today's date
 
-		# get UTC timestamp from seconds since epoch
-	    #utc_dt = datetime.utcfromtimestamp(closure.created_at_in_seconds).replace(tzinfo=pytz.utc)
-	    #print('utc: {}'.format(utc_dt))
-	    # convert to local time in the user's timezone
-	    #localtime_dt = utc_dt.astimezone(localtime_tz)
-	    #print('localtime [{}]: {}'.format(localtime_dt.tzname(), localtime_dt))
-
-		#print closure
+		print closure
 		# # need to convert tweet time/date from UTC to Pacific time
-		twt_text = closure[-1]
-		twt_time = closure[0]
+		twt_time = closure[0]    # First item from response is timestamp
+		twt_text = closure[-1]   # Last item is tweet text
 
+		# Convert tweet time stamp from UTC to local time
 		twt_local_time = to_local_time(twt_time)
-		# print twt_text
-		# print twt_time
-		# print twt_local_time
 
-		# convert twt time to date object for compariosn
-		timestring = ' '.join(twt_local_time.split(" ")[:-2])
-		print timestring
-		timestring = timestring + " " + timestring.split(" ")[-1]
-		print timestring
-		twt_datetime = datetime.strptime(timestring, '%a %b %d %H:%M:%S %Y')
+		# Convert tweet string to datetime object
+		twt_datetime = datetime.strptime(twt_local_time, '%a %b %d %H:%M:%S %Y')
 
-		#	compare to start and end times
+		# Compare to start and end times
 		# consider only tweets that are within past 30 minutes for now
 		# get current time and date
 		present = datetime.now()
 
 		dt = present - twt_datetime
-		#date_object = datetime.strptime(twt_local_time, '%d %M %Y %I:%M%p')
 		newdt = days_hours_minutes(dt)
 
 		timestamp_min = 30
 
 		# only report if difference is in past 30 minutes
-		if newdiff['days'] == 0 and newdiff['hours'] == 0 and newdiff['minutes'] < timestamp_min:
-		    print 'opening within past 30 minutes'
+		if newdt['days'] == 0 and newdt['hours'] == 0 and newdt['minutes'] < timestamp_min:
+		    print 'bridge opening within past 30 minutes'
+		    
 		    return closure
 
-		# 		return closure
+def sendAlert(closures, email):
+	# Import the email modules we'll need
+	from email.mime.text import MIMEText
 
-# def sendAlert(closures, email):
-# 	# Import the email modules we'll need
-# 	from email.mime.text import MIMEText
+	# Open a plain text file for reading.  For this example, assume that
+	# the text file contains only ASCII characters.
+	# fp = open(textfile, 'rb')
+	# Create a text/plain message
+	msg = MIMEText('some contents')
+	# fp.close()
 
-# 	# Open a plain text file for reading.  For this example, assume that
-# 	# the text file contains only ASCII characters.
-# 	# fp = open(textfile, 'rb')
-# 	# Create a text/plain message
-# 	msg = MIMEText('some contents')
-# 	# fp.close()
+	# me == the sender's email address
+	# you == the recipient's email address
+	msg['Subject'] = 'Test Email'
+	msg['From'] = 'you'
+	msg['To'] = 'person'
 
-# 	# me == the sender's email address
-# 	# you == the recipient's email address
-# 	msg['Subject'] = 'Test Email'
-# 	msg['From'] = 'you'
-# 	msg['To'] = 'person'
+	# Send the message via our own SMTP server, but don't include the
+	# envelope header.
+	s = smtplib.SMTP('localhost:8888')
+	s.sendmail(me, [you], msg.as_string())
+	s.quit()
 
-# 	# Send the message via our own SMTP server, but don't include the
-# 	# envelope header.
-# 	s = smtplib.SMTP('localhost:8888')
-# 	s.sendmail(me, [you], msg.as_string())
-# 	s.quit()
+
 
 def main():
 	handle = '@SDOTbridges'
-	closures = filterClosures('Spokane', handle=handle)
 	
-	# Get the members of tamtar's list "Things That Are Rad"
-	# print api.lists.members(owner_screen_name=handle, slug="Spokane")
+	# only run check during a specified time interval
+	# should be arguments set by user
+	closure = filterClosures('Spokane', handle=handle)
+	
+	if closure:
 
 
 	# print closures
