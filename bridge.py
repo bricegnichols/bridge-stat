7e6f44048e62f02c173cdf15e18e10f31753c967
Your Name <you@example.com>
2015-08-08 15:07:32 -0700

    rewrote script to use streaming function, simplified process, sends email for specific closure now

--- bridge.py
+++ bridge.py
@@ -10,118 +10,70 @@ from email.mime.text import MIMEText
 from datetime import datetime
 from dateutil.tz import tzoffset
 import sched, time
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import StreamListener
+from tweepy import Stream
+import json
 
 api = twitter.Api(consumer_key=consumer_key,
 	consumer_secret=consumer_secret,
 	access_token_key=access_token_key,
 	access_token_secret=access_token_secret)
 
-# Check recent statuses and store any for Spokane St.
+twitterID = '2768116808'
+bridgeName = 'Spokane'
 
-def getClosures(bridgeName, statuses):
-	closures = []
-	for status in statuses:
-		if bridgeName in status.text and "closed" in status.text:
-		    closures.append([status.created_at, status.text])
-
-	return closures
-
-def to_local_time(tweet_time_string):
-    """Convert rfc 5322 -like time string into a local time
-       string in rfc 3339 -like format.
-
+class StdOutListener(StreamListener):
+    """ A listener handles tweets are the received from the stream.
+    This is a basic listener that just prints received tweets to stdout.
     """
-    timestamp = email.utils.mktime_tz(email.utils.parsedate_tz(tweet_time_string))
-    
-    return datetime.fromtimestamp(timestamp, pytz.timezone(local_tz)).strftime('%a %b %d %H:%M:%S %Y')
+    def on_data(self, data):
+   	    process_tweet(data)
+        #return True
 
-def days_hours_minutes(td):
-    mydict = {}
-    mydict['days'] = td.days
-    mydict['hours'] = td.seconds//3600
-    mydict['minutes'] = (td.seconds//60)%60
-    
-    return mydict
+    def on_error(self, status):
+        print(status)
 
-def filterClosures(bridgeName, handle):
-	statuses = api.GetUserTimeline(screen_name=handle,count=50)
-	closures = getClosures(bridgeName, statuses)
-	currentDate = datetime.now()
+def process_tweet(data):
+    decoded = json.loads(data)
 
-	for status in statuses:
-		#print status
-		print status.created_at
-		# tweet_time = status.created_at
-		# local_time = to_local_time(tweet_time)
-		# print time_string
+    twt_text = decoded['text']
+    # Do some stuff with the json data if tweet is a closure for specific bridge
+    if bridgeName in twt_text and "closed" in twt_text:
+    	# Send and email or turn on a light or something
+    	print "BRIDGE CLOSED!!! Eat another piece of toast, pet the cats"
+    	sendAlert(data=decoded, email=to_emailAddress)
 
-	for closure in closures:
-		#Get closures from today's date
+    return True
 
-		print closure
-		# # need to convert tweet time/date from UTC to Pacific time
-		twt_time = closure[0]    # First item from response is timestamp
-		twt_text = closure[-1]   # Last item is tweet text
-
-		# Convert tweet time stamp from UTC to local time
-		twt_local_time = to_local_time(twt_time)
-
-		# Convert tweet string to datetime object
-		twt_datetime = datetime.strptime(twt_local_time, '%a %b %d %H:%M:%S %Y')
-
-		# Compare to start and end times
-		# consider only tweets that are within past 30 minutes for now
-		# get current time and date
-		present = datetime.now()
-
-		dt = present - twt_datetime
-		newdt = days_hours_minutes(dt)
-
-		timestamp_min = 30
-
-		# only report if difference is in past 30 minutes
-		if newdt['days'] == 0 and newdt['hours'] == 0 and newdt['minutes'] < timestamp_min:
-		    print 'bridge opening within past 30 minutes'
-		    
-		    return closure
-
-def sendAlert(closures, email):
+def sendAlert(data, email):
 	# Import the email modules we'll need
-	from email.mime.text import MIMEText
+	fromaddr = email_address
+	toaddrs = emailAddress
+	msg = 'The bridge just opened...'
+	username = email_address
+	password = email_password
 
-	# Open a plain text file for reading.  For this example, assume that
-	# the text file contains only ASCII characters.
-	# fp = open(textfile, 'rb')
-	# Create a text/plain message
-	msg = MIMEText('some contents')
-	# fp.close()
-
-	# me == the sender's email address
-	# you == the recipient's email address
-	msg['Subject'] = 'Test Email'
-	msg['From'] = 'you'
-	msg['To'] = 'person'
-
-	# Send the message via our own SMTP server, but don't include the
-	# envelope header.
-	s = smtplib.SMTP('localhost:8888')
-	s.sendmail(me, [you], msg.as_string())
-	s.quit()
+	server = smtplib.SMTP('smtp.gmail.com:587')
+	server.ehlo()
+	server.starttls()
+	server.login(username,password)
+	server.sendmail(fromaddr, toaddrs, msg)
+	server.quit()
 
 
-
-def main():
-	handle = '@SDOTbridges'
-	
-	# only run check during a specified time interval
-	# should be arguments set by user
-	closure = filterClosures('Spokane', handle=handle)
-	
-	if closure:
-
-
-	# print closures
-	#sendAlert(closures, email)
 
 if __name__ == '__main__':
-	main()
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token_key, access_token_secret)
+
+    stream = Stream(auth, l)
+    # Stream tweets live
+
+    # follow only the SDOTbridges twitter ID
+    stream.filter(follow=[twitterID])
+
+    # test with a general feed for now
+    #stream.filter(track=['programming'])
