b7fe68b4d3ba2b8a950c8511bee7e5114b758c27
Your Name <you@example.com>
2015-08-08 10:53:09 -0700

    edits for time comparison

--- bridge.py
+++ bridge.py
@@ -1,10 +1,13 @@
 # get Seattle bridge open/close status from Twitter 
 import twitter
+import pytz
 import datetime
 from config import *
 import smtplib
 import email
 from email.mime.text import MIMEText
+from datetime import datetime
+from dateutil.tz import tzoffset
 
 api = twitter.Api(consumer_key=consumer_key,
 	consumer_secret=consumer_secret,
@@ -17,68 +20,114 @@ def getClosures(bridgeName, statuses):
 	closures = []
 	for status in statuses:
 		if bridgeName in status.text and "closed" in status.text:
-		    closures.append(status.created_at.split(' ')[1:4] + [status.text])
+		    closures.append([status.created_at, status.text])
 
 	return closures
 
-def filterClosures(bridgeName, startTime, endTime, handle):
+def to_local_time(tweet_time_string):
+    """Convert rfc 5322 -like time string into a local time
+       string in rfc 3339 -like format.
+
+    """
+    timestamp = email.utils.mktime_tz(email.utils.parsedate_tz(tweet_time_string))
+    return datetime.fromtimestamp(timestamp, pytz.timezone(local_tz)).strftime('%Y-%m-%d %H:%M:%S')
+
+def days_hours_minutes(td):
+    mydict = {}
+    mydict['days'] = td.days
+    mydict['hours'] = td.seconds//3600
+    mydict['minutes'] = (td.seconds//60)%60
+    return mydict
+
+def filterClosures(bridgeName, handle):
 	statuses = api.GetUserTimeline(screen_name=handle,count=50)
 	closures = getClosures(bridgeName, statuses)
-	currentDate = datetime.datetime.now()
+	currentDate = datetime.now()
+
+	# for status in statuses:
+	# 	print status
+	# 	print status.created_at
+	# 	tweet_time = status.created_at
+	# 	local_time = to_local_time(tweet_time)
+	# 	print time_string
+
 	for closure in closures:
-		# Get closures from today's date
-		# print closure
-		if closure[1] == str(currentDate.day):
-			# Extract time from tweet text, created_at time is inaccurate
-			text = closure[-1]
-			time = text.split('- ')[-1]
+		#Get closures from today's date
 
-			# compare to start and end times
-			# Start and End times should be spec'ed as [hr, min, am/pm]
-			# Split input start and end times
-			startHour = startTime[0]
-			startMin = startTime[1]
-			startSun = startTime[-1]
+		# get UTC timestamp from seconds since epoch
+	    #utc_dt = datetime.utcfromtimestamp(closure.created_at_in_seconds).replace(tzinfo=pytz.utc)
+	    #print('utc: {}'.format(utc_dt))
+	    # convert to local time in the user's timezone
+	    #localtime_dt = utc_dt.astimezone(localtime_tz)
+	    #print('localtime [{}]: {}'.format(localtime_dt.tzname(), localtime_dt))
 
-			endHour = endTime[0]
-			endMin = endTime[1]
-			endSun = endTime[-1]
+		#print closure
+		# # need to convert tweet time/date from UTC to Pacific time
+		twt_text = closure[-1]
+		twt_time = closure[0]
 
-			# # Split tweet times
-			tweetHour = int(time.split(':')[0])
-			tweetMin = int(time.split(':')[1])
-			tweetSun = time.split(' ')[-1]
+		twt_local_time = to_local_time(twt_time)
+		# print twt_text
+		# print twt_time
+		# print twt_local_time
 
-			if startHour < tweetHour < endHour:
-				# also need to account for minute differences...
-				return closures
+		# convert twt time to date object for compariosn
+		timestring = ' '.join(twt_local_time.split(" ")[:-2])
+		print timestring
+		timestring = timestring + " " + timestring.split(" ")[-1]
+		print timestring
+		twt_datetime = datetime.strptime(timestring, '%a %b %d %H:%M:%S %Y')
 
-def sendAlert(closures, email):
-	# Import the email modules we'll need
-	from email.mime.text import MIMEText
+		#	compare to start and end times
+		# consider only tweets that are within past 30 minutes for now
+		# get current time and date
+		present = datetime.now()
 
-	# Open a plain text file for reading.  For this example, assume that
-	# the text file contains only ASCII characters.
-	# fp = open(textfile, 'rb')
-	# Create a text/plain message
-	msg = MIMEText('some contents')
-	# fp.close()
+		dt = present - twt_datetime
+		#date_object = datetime.strptime(twt_local_time, '%d %M %Y %I:%M%p')
+		newdt = days_hours_minutes(dt)
 
-	# me == the sender's email address
-	# you == the recipient's email address
-	msg['Subject'] = 'Test Email'
-	msg['From'] = 'you'
-	msg['To'] = 'person'
+		timestamp_min = 30
 
-	# Send the message via our own SMTP server, but don't include the
-	# envelope header.
-	s = smtplib.SMTP('localhost:8888')
-	s.sendmail(me, [you], msg.as_string())
-	s.quit()
+		# only report if difference is in past 30 minutes
+		if newdiff['days'] == 0 and newdiff['hours'] == 0 and newdiff['minutes'] < timestamp_min:
+		    print 'opening within past 30 minutes'
+		    return closure
+
+		# 		return closure
+
+# def sendAlert(closures, email):
+# 	# Import the email modules we'll need
+# 	from email.mime.text import MIMEText
+
+# 	# Open a plain text file for reading.  For this example, assume that
+# 	# the text file contains only ASCII characters.
+# 	# fp = open(textfile, 'rb')
+# 	# Create a text/plain message
+# 	msg = MIMEText('some contents')
+# 	# fp.close()
+
+# 	# me == the sender's email address
+# 	# you == the recipient's email address
+# 	msg['Subject'] = 'Test Email'
+# 	msg['From'] = 'you'
+# 	msg['To'] = 'person'
+
+# 	# Send the message via our own SMTP server, but don't include the
+# 	# envelope header.
+# 	s = smtplib.SMTP('localhost:8888')
+# 	s.sendmail(me, [you], msg.as_string())
+# 	s.quit()
 
 def main():
 	handle = '@SDOTbridges'
-	closures = filterClosures('Spokane', startTime=[1,12,"PM"], endTime=[3,00,"PM"], handle=handle)
+	closures = filterClosures('Spokane', handle=handle)
+	
+	# Get the members of tamtar's list "Things That Are Rad"
+	# print api.lists.members(owner_screen_name=handle, slug="Spokane")
+
+
+	# print closures
 	#sendAlert(closures, email)
 
 if __name__ == '__main__':
