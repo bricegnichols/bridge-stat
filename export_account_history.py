56eeec95c54fc65a27b3d30ee4f6f193bacfab1c
Your Name <you@example.com>
2016-01-30 19:24:04 -0800

    date stamp for file name

--- export_account_history.py
+++ export_account_history.py
@@ -6,13 +6,15 @@
 
 import tweepy 
 import csv
+from datetime import datetime
+from config import *
 
 def get_all_tweets(screen_name):
 	#Twitter only allows access to a users most recent 3240 tweets with this method
 	
 	#authorize twitter, initialize tweepy
 	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-	auth.set_access_token(access_key, access_secret)
+	auth.set_access_token(access_token_key, access_token_secret)
 	api = tweepy.API(auth)
 	
 	#initialize a list to hold all the tweepy Tweets
@@ -45,8 +47,9 @@ def get_all_tweets(screen_name):
 	#transform the tweepy tweets into a 2D array that will populate the csv	
 	outtweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in alltweets]
 	
-	#write the csv	
-	with open('%s_tweets.csv' % screen_name, 'wb') as f:
+	#write the csv
+	fname = str(datetime.now().month)+'-'+str(datetime.now().day)+'-'+str(datetime.now().year)	
+	with open(fname + '.csv', 'wb') as f:
 		writer = csv.writer(f)
 		writer.writerow(["id","created_at","text"])
 		writer.writerows(outtweets)
