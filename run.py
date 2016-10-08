7b2f23847cf17b0eb2f7c4919f4db2d81a0fc75d
Brice Nichols <bricenichols@gmail.com>
2016-10-08 16:49:49 -0700

    whoops typo

--- run.py
+++ run.py
@@ -55,4 +55,4 @@ def map():
 
 if __name__ == '__main__':
 	# app.run(debug=True)
-	run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
+	app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
